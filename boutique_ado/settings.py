72d15a0cbedd603a36a4c8ae47fc8e7b8b0945db
chellej80 <embarrequestrian@gmail.com>
2022-10-11 19:59:15 +0000

    updated webhooks

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
 import os
+
+from dotenv import load_dotenv 
+load_dotenv() 
+
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -100,6 +105,8 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
